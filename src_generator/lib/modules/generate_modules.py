12ba9d68e2ccc12ae51f331a879a6da57823c602
Rossi, Giacomo <giacombum@gmail.com>
2022-02-16 11:23:14 +0000

    Add logical routines

--- src_generator/lib/modules/generate_modules.py
+++ src_generator/lib/modules/generate_modules.py
@@ -11,7 +11,7 @@ def render(tpl_path, context):
 
 ####
 
-types=['integer', 'real', 'complex']
+types=['integer', 'real', 'complex', 'logical']
 
 b_1 ={'name':'8',   'key':'1P',  'name_n':8}
 b_2 ={'name':'16',  'key':'2P',  'name_n':16}
@@ -21,11 +21,13 @@ b_16={'name':'128', 'key':'16P', 'name_n':128}
 
 kinds={'integer': [b_1, b_2, b_4, b_8],
        'real'   : [          b_4, b_8, b_16],
-       'complex': [          b_4, b_8, b_16]}
+       'complex': [          b_4, b_8, b_16],
+       'logical': [          b_4]}
 
 keywords={'integer': ['Integer', 'int',   'I'],
           'real'   : ['Real',    'real',  'R'],
-          'complex': ['Complex', 'cmplx', 'R']}
+          'complex': ['Complex', 'cmplx', 'R'],
+          'logical': ['Logical', 'lgcl',  'I']}
 
 dimensions=7
 
