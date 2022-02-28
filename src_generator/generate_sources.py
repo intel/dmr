8a50ea47169ac41c4a7b98f7c005997388314b94
Rossi, Giacomo <giacombum@gmail.com>
2022-02-28 16:19:11 +0000

    Use one single Python generator for both modules and submodules

--- src_generator/generate_sources.py
+++ src_generator/generate_sources.py
@@ -13,10 +13,10 @@ def render(tpl_path, context):
 
 types=['integer', 'real', 'complex', 'logical']
 
-b_1 ={'name':'8',   'key':'1P'}
-b_2 ={'name':'16',  'key':'2P'}
-b_4 ={'name':'32',  'key':'4P'}
-b_8 ={'name':'64',  'key':'8P'}
+b_1 ={'name':'8',   'key':'1P' }
+b_2 ={'name':'16',  'key':'2P' }
+b_4 ={'name':'32',  'key':'4P' }
+b_8 ={'name':'64',  'key':'8P' }
 b_16={'name':'128', 'key':'16P'}
 
 kinds={'integer': [b_1, b_2, b_4, b_8],
@@ -31,52 +31,62 @@ keywords={'integer': ['Integer', 'int',   'I'],
 
 dimensions=7
 
-with open('dmr_target_free.F90', 'w') as f:
-    f.write(render('dmr_target_free.jf90',
+with open('lib/submodules/dmr_target_free.F90', 'w') as f:
+    f.write(render('lib/submodules/dmr_target_free.jf90',
       {'types' : types, 'kinds' : kinds, 'keywords' : keywords}
                   ))
 
-with open('dmr_target_is_present.F90', 'w') as f:
-    f.write(render('dmr_target_is_present.jf90',
+with open('lib/submodules/dmr_target_is_present.F90', 'w') as f:
+    f.write(render('lib/submodules/dmr_target_is_present.jf90',
       {'types' : types, 'kinds' : kinds, 'keywords' : keywords}
                   ))
 
-with open('dmr_target_alloc.F90', 'w') as f:
-    f.write(render('dmr_target_alloc.jf90',
+with open('lib/submodules/dmr_target_alloc.F90', 'w') as f:
+    f.write(render('lib/submodules/dmr_target_alloc.jf90',
       {'types' : types, 'kinds' : kinds, 'keywords' : keywords, 'dimensions' : dimensions}
                   ))
 
-with open('dmr_correctly_mapped.F90', 'w') as f:
-    f.write(render('dmr_correctly_mapped.jf90',
+with open('lib/submodules/dmr_correctly_mapped.F90', 'w') as f:
+    f.write(render('lib/submodules/dmr_correctly_mapped.jf90',
       {'types' : types, 'kinds' : kinds, 'keywords' : keywords, 'dimensions' : dimensions}
                   ))
 
-with open('dmr_get_mapped_ptr.F90', 'w') as f:
-    f.write(render('dmr_get_mapped_ptr.jf90',
+with open('lib/submodules/dmr_get_mapped_ptr.F90', 'w') as f:
+    f.write(render('lib/submodules/dmr_get_mapped_ptr.jf90',
       {'types' : types, 'kinds' : kinds, 'keywords' : keywords, 'dimensions' : dimensions}
                   ))
 
-with open('dmr_target_init.F90', 'w') as f:
-    f.write(render('dmr_target_init.jf90',
+with open('lib/submodules/dmr_target_init.F90', 'w') as f:
+    f.write(render('lib/submodules/dmr_target_init.jf90',
       {'types' : types, 'kinds' : kinds, 'keywords' : keywords, 'dimensions' : dimensions}
                   ))
 
-with open('dmr_device_memcpy.F90', 'w') as f:
-    f.write(render('dmr_device_memcpy.jf90',
+with open('lib/submodules/dmr_device_memcpy.F90', 'w') as f:
+    f.write(render('lib/submodules/dmr_device_memcpy.jf90',
       {'types' : types, 'kinds' : kinds, 'keywords' : keywords, 'dimensions' : dimensions}
                   ))
 
-with open('dmr_target_memcpy.F90', 'w') as f:
-    f.write(render('dmr_target_memcpy.jf90',
+with open('lib/submodules/dmr_target_memcpy.F90', 'w') as f:
+    f.write(render('lib/submodules/dmr_target_memcpy.jf90',
       {'types' : types, 'kinds' : kinds, 'keywords' : keywords, 'dimensions' : dimensions}
                   ))
 
-with open('dmr_target_memcpy_rect.F90', 'w') as f:
-    f.write(render('dmr_target_memcpy_rect.jf90',
+with open('lib/submodules/dmr_target_memcpy_rect.F90', 'w') as f:
+    f.write(render('lib/submodules/dmr_target_memcpy_rect.jf90',
       {'types' : types, 'kinds' : kinds, 'keywords' : keywords, 'dimensions' : dimensions}
                   ))
 
-with open('dmr_target_memcpy_scalar.F90', 'w') as f:
-    f.write(render('dmr_target_memcpy_scalar.jf90',
+with open('lib/submodules/dmr_target_memcpy_scalar.F90', 'w') as f:
+    f.write(render('lib/submodules/dmr_target_memcpy_scalar.jf90',
+      {'types' : types, 'kinds' : kinds, 'keywords' : keywords, 'dimensions' : dimensions}
+                  ))
+
+with open('lib/modules/dmr_environment.F90', 'w') as f:
+    f.write(render('lib/modules/dmr_environment.jf90',
+      {'types' : types[0:2], 'kinds' : kinds, 'keywords' : keywords, 'dimensions' : dimensions}
+                  ))
+
+with open('lib/modules/dmr.F90', 'w') as f:
+    f.write(render('lib/modules/dmr.jf90',
       {'types' : types, 'kinds' : kinds, 'keywords' : keywords, 'dimensions' : dimensions}
                   ))
