81f8c9db380d68c2b34fcead11ccd08e7925bb89
Giacomo Rossi <giacombum@gmail.com>
2022-04-13 01:04:44 -0700

    Add Cray compiler support

---
+++ cmake/CrayFortranCompiler.cmake
@@ -0,0 +1,32 @@
+include(CheckFortranCompilerFlag)
+
+if(CMAKE_Fortran_COMPILER_VERSION VERSION_LESS 12.0.0)
+	message(FATAL_ERROR "Compiler Version ${CMAKE_Fortran_COMPILER_VERSION}. "
+        "GPU acceleration requires Cray Fortran version 12.0.0 or higher ")
+endif()
+
+set(CRAYFORTRAN_COMPILE_OPTIONS)
+set(CRAYFORTRAN_LINK_OPTIONS)
+set(CRAYFORTRAN_OPTIONS)
+
+set(CRAYFORTRAN_COMPILE_OPTIONS "-fopenmp")
+
+list(APPEND CRAYFORTRAN_OPTIONS ${CRAYFORTRAN_COMPILE_OPTIONS})
+
+message("   ftn related compile and link options: ${CRAYFORTRAN_OPTIONS}")
+set(CMAKE_REQUIRED_LINK_OPTIONS ${CRAYFORTRAN_OPTIONS})
+check_fortran_compiler_flag("${CRAYFORTRAN_OPTIONS}" CRAYFORTRAN_VALID_OPTIONS)
+unset(CMAKE_REQUIRED_LINK_OPTIONS)
+if(NOT CRAYFORTRAN_VALID_OPTIONS)
+	unset(CRAYFORTRAN_VALID_OPTIONS CACHE)
+message(FATAL_ERROR "ftn related option check failed! "
+					"Please check CMakeError.log for the exact error.")
+endif()
+
+add_library(compilerCustomConfig INTERFACE)
+target_compile_options(compilerCustomConfig
+	INTERFACE
+		${CRAYFORTRAN_COMPILE_OPTIONS})
+target_link_options(compilerCustomConfig
+	INTERFACE
+		${CRAYFORTRAN_LINK_OPTIONS})
